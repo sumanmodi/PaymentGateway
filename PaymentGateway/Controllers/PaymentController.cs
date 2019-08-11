d48667c9d3ded43a74fa1de34e55249e078c3c5c
Suman Modi <sumanmodi@live.com>
2019-08-11 08:18:15 +0100

    further files added

---
+++ PaymentGateway/Controllers/PaymentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string transactionId)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string cardNumber, string expiryMonthYear, double paymentAmount, string currencyCode, string CVVCode )
+        {
+
+        }
+
+        
+    }
+}
